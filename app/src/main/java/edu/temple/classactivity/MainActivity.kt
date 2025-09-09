3df12eb7c2c1672b89a4213bc0385aab705c1257
Bao Huynh <tur71867@temple.edu>
2025-09-09 12:06:45 -0400

    Added button click for name and if does not enter name prompt to enter from text view

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,35 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var myButton: Button
+    lateinit var displayMessage : TextView
+    lateinit var editInput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById<Button>(R.id.clickButton)
+        displayMessage = findViewById<TextView>(R.id.textHello)
+        editInput = findViewById<EditText>(R.id.editName)
+
+        myButton.setOnClickListener {
+            if (editInput.text.toString() != ""){
+                displayMessage.text = "Hello, ${editInput.text}"
+                editInput.text.clear()
+            }
+            else{
+                displayMessage.text = "Please enter something"
+            }
+            }
+        }
+
     }
-}
